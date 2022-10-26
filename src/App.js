cbb96d2c9aae85c76090e7ff1b9fc2bdaccc798a
larsingerlars <64693316+larsingerlars@users.noreply.github.com>
2022-10-26 18:11:21 +0300

    FEAT: app/index basic files + styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Components
+import CurrencySelection from "./components/ui/CurrencySelection";
+import Header from "./components/ui/Header";
+import Logo from "./components/Logo";
+import Navbar from "./components/ui/Navbar";
+import ShowCart from "./components/cart/ShowCart";
+import ProductsOverview from "./components/products/ProductsOverview";
+
+// DummyData
+import { dummyData } from "./data/dummyData/productsData";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Header title="Header">
+          <Navbar />
+          <Logo />
+          <div className="header__ui">
+            <CurrencySelection />
+            <ShowCart />
+          </div>
+        </Header>
+        <main className="container">
+          <Routes>
+            <Route
+              path="/"
+              element={<ProductsOverview products={dummyData[3]} />}
+            />
+            <Route
+              path="/women"
+              element={<ProductsOverview products={dummyData[0]} />}
+            />
+            <Route
+              path="/men"
+              element={<ProductsOverview products={dummyData[1]} />}
+            />
+            <Route
+              path="/kids"
+              element={<ProductsOverview products={dummyData[2]} />}
+            />
+          </Routes>
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
